6767330203ef276833d00a7e72ac5c36e4b07db4
Maria Lagunero <maria58@gmail.com>
2020-04-10 08:36:51 -0700

    add beginnings of business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 //business logic
+//1 constructor
+function Pizza()
+//2properties of constructor
+toppings, size
+//3objects toppings, size, cost,?ticket and disply?
+//4create prototypes to display "receipt" of pizza made.
+
 
 
 

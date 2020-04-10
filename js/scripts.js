995b0474a44f51c23afc668d5782ca415fe1fbf6
Maria Lagunero <maria58@gmail.com>
2020-04-10 14:58:01 -0700

    add id to show completed pizza order. still working

--- js/scripts.js
+++ js/scripts.js
@@ -11,19 +11,24 @@ function Pizza(toppings, size) {
 
 Pizza.prototype.displayPrice = function () {
   if (this.size === 1){
-    this.price = 8.00;
-  }else{
-    this.price = 5.00;
+    this.price = 6.00;
+  }else {
+    this.price = 10.00;
   }
 }
 
 Pizza.prototype.displayPizza = function () {
 //print all var here?//
+var toppings = toppings;
+var size = size;
+var price = this.price;
 //concat/join all properties//
+$("#showPizza").html("<p> Toppings" + toppings + "Size" + size + "Price:" + price + "</p>");
+$("#showPizza").show();
 }
 
 
-//3 toppings(bacon,peppers,etc), size(s,m,l), cost(3 costs 6, 10, 14),?receipt and disply?
+//3 toppings(bacon,peppers,etc), size(s,m,l), cost(3 costs 6, 10, 14),?receipt and display?
 //4 create prototypes to display receipt of pizza made.
 
 
@@ -47,6 +52,8 @@ $(document).ready(function(){
     console.log(pizza);
 
     //remember to display on html side//
+    pizza.displayPrice();
+    pizza.displayPizza();
 
   });
 
